659e01ab1a75a99e105abb2e36fa3692a452fb64
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2026-01-27 20:15:25 +0530

    Implement login functionality with JWT and cookie handling

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,12 +1,14 @@
 import { Router } from "express";
 import{
-    registerUser
+    registerUser,
+    loginUser
 } from "../controllers/user.controller.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
 const router = Router()
 
 router.post("/register", registerUser);
+router.route("/login").post(loginUser);
 
 
 export default router;
