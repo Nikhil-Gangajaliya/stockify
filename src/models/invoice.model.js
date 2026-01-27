09bdb0b2fa1426b61720f68a47dc467d68fa9eed
Nikhil Gangajaliya <nikhilgangajaliya5@gmail.com>
2026-01-27 18:12:06 +0530

    Add Invoice and Order models with schemas and timestamps

---
+++ src/models/invoice.model.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const invoiceSchema = new Schema(
+    {
+        order: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Order",
+      required: true,
+    },
+
+    invoiceNumber: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+
+    amount: {
+      type: Number,
+      required: true,
+    },
+
+    paymentMode: {
+      type: String,
+      enum: ["cash", "upi", "card"],
+      required: true,
+    },
+
+    paidAt: {
+      type: Date,
+      default: Date.now,
+    }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Invoice = mongoose.model("Invoice", invoiceSchema);
---
+++ src/models/order.model.js
@@ -0,0 +1,45 @@
+import mongoose, { Mongoose, Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const orderSchema = new Schema(
+    {
+        store: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Store",
+            required: true
+        },
+
+        user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+
+        items: [
+            {
+                product: {
+                    type: mongoose.Schema.Types.ObjectId,
+                    ref: "Product"
+                },
+                quantity: Number,
+                price: Number
+            },
+        ],
+
+        totalAmount: {
+            type: Number,
+            required: true
+        },
+
+        status: {
+            type: String,
+            enum: ["pending", "completed", "cancelled"],
+            default: "pending"
+        }
+    },
+    {
+        timestamps: true
+    }
+);
+
+export const Order = mongoose.model("Order", orderSchema);
